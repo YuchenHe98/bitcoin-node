63b80852174283189e3a9ac88577211024384f11
YuchenHe98 <31009947+YuchenHe98@users.noreply.github.com>
2019-07-01 14:55:57 +0800

    prototype without exception handling

--- generate_result.js
+++ generate_result.js
@@ -1,87 +1,109 @@
 import React from 'react';
 import 'antd/dist/antd.css';
+import { Divider } from 'antd';
+import { Table } from 'antd';
 import { Tabs } from 'antd';
 import { Input } from 'antd';
 
 const { TabPane } = Tabs;
 
 function callback(key) {
-  console.log(key);
+    console.log(key);
+}
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key];
+        var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        var data = require('./data.json');
         this.state = {
-            columnName: '',
-            query: '',
-            totalAmount: 0,
-            numAmount: 0,
+            keyColumn: '',
+            targetColumn: '',
         }
-        this.handleQuery = this.handleQuery.bind(this);
-        this.chooseColumn = this.chooseColumn.bind(this);
-      
+        this.handleKey = this.handleKey.bind(this);
+        this.handleTarget = this.handleTarget.bind(this);
     }
 
-    chooseColumn(event) {
-        var columnName = event.target.value;
+    handleKey(event) {
+        var keyColumn = event.target.value;
         this.setState({
-            columnName: columnName
+            keyColumn: keyColumn
         });
     }
-
-    handleQuery(event) {
-        const data = require('./data.json');
-        var totalAmount = 0;
-        var numAmount = 0;
-        var query = event.target.value;
-        for (var i = 0; i < data.length; i++) {
-            if (this.state.columnName === '' || query === '' || data[i][this.state.columnName] === query) {
-                totalAmount += data[i].amount;
-                numAmount += 1;
-            }
-        }
-
+  
+   handleTarget(event){
+       var targetColumn = event.target.value;
         this.setState({
-            query: query,
-            totalAmount: totalAmount,
-            numAmount: numAmount,
+            targetColumn: targetColumn
         });
     }
 
     render() {
+        const columns = [
+            {
+                title: this.state.keyColumn,
+                dataIndex: this.state.keyColumn,
+                key: this.state.keyColumn
+            },
+            {
+                title: 'Number of instances',
+                dataIndex: 'num',
+                key: 'num',
+            },
+            {
+                title: 'Mean',
+                dataIndex: 'mean',
+                key: 'mean'
+            },
+            {
+                title: 'Standard deviation',
+                dataIndex: 'stdDev',
+                key: 'stdDev'
+            }
+        ];
+
+        const allData = require('./data.json');
+        var dataMap = {};
+        for (var i = 0; i < allData.length; i++) {
+            var currentKey = allData[i][this.state.keyColumn];
+            if (!(currentKey in dataMap)) {
+                dataMap[currentKey] = [];
+            }
+            var currentValue = allData[i][this.state.targetColumn];
+            dataMap[currentKey].push(currentValue);
+        }
+      
+      
+        var data = [];
+        for (var key in dataMap) {
+            var currentRow = {};
+            currentRow[this.state.keyColumn] = key;
+            const currentTarget = dataMap[key];
+            const n = currentTarget.length;
+            const mean = currentTarget.reduce((a,b) => a+b)/n;
+            const stdDev = Math.sqrt(currentTarget.map(x => Math.pow(x-mean,2)).reduce((a,b) => a+b)/n);
+            currentRow['mean'] = mean;
+            currentRow['num'] = n;
+            currentRow['stdDev'] = stdDev;
+            data.push(currentRow);
+        }
+
         return (
     <div className="App">
         <header className="App-header">
         <div className="example-input">
-            <Input placeholder="key type" value={this.state.columnName} onChange={this.chooseColumn}/>
-            <Input placeholder="coin type" value={this.state.query} onChange={this.handleQuery}/>
+            <Input placeholder="key type" value={this.state.keyColumn} onChange={this.handleKey}/>
+            <Input placeholder="target type" value={this.state.targetColumn} onChange={this.handleTarget}/>
         </div>
 
-        <p>
-            Column {this.state.columnName} is queried, and the query is {this.state.query}!
-        </p>
+        <Table columns={columns} dataSource={data} />
 
-        <Tabs defaultActiveKey="1" onChange={callback}>
-            <TabPane tab="Num Trades" key="1">
-                {this.state.numAmount}
-            </TabPane>
-
-            <TabPane tab="Total Amount" key="2">
-                {this.state.totalAmount}
-            </TabPane>
-          
-        </Tabs>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         </header>
     </div>
         );
